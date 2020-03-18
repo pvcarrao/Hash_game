fa5cb415bc88214d5068edb2aeb92d6b3af2c229
pvcarrao <pvcarrao@gmail.com>
2020-03-18 20:35:05 -0300

    fixing db

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ from main.game import Game
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-Game.create_database
+Game().create_database()
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,7 +16,7 @@ def home():
 
 @app.route('/game', methods=['POST'])
 def new_game():
-    resp = Game.new_game()
+    resp = Game().new_game()
     return jsonify(resp)
 
 @app.route('/game/{id}/movement', methods=['POST'])
--- main/game.py
+++ main/game.py
@@ -6,16 +6,18 @@ class Game:
     def __init__(self):
         pass
 
-    def create_database():
-        conn = sqlite3.connect()
+    def create_database(self):
+        conn = sqlite3.connect(database="database")
         c = conn.cursor()
         table_name = 'games'
         sql = 'create table if not exists ' + table_name + ' (id integer)'
         c.execute(sql)
         conn.commit()
 
-    def new_game():
+    def new_game(self):
         game_id = "1"
         player = random.choice(["X", "O"])
         response = [game_id, player]
         return response
+
+
