c114eaf05413b466495ac4ad2e9d29a4314e14d2
Pedro Vitor Ramos Halbout Carrão <pvcarrao@gmail.com>
2020-03-18 19:17:54 -0300

    Create api.py

---
+++ api.py
@@ -0,0 +1,26 @@
+import flask
+from flask import request, jsonify
+
+from game import Game
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Bem vindo ao jogo da velha da galera </h1><p>Readme</p>"
+
+@app.route('/game', methods=['POST'])
+def new_game():
+    resp = Game.new_game()
+    return jsonify(resp)
+
+@app.route('/game/{id}/movement', methods=['POST'])
+def movement():
+    # try:
+
+    # except GameDoesNotExist:
+
+    return
+
+app.run()
