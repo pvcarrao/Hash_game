7f7408504374c4974903808d475a8d932b455edb
pvcarrao <pvcarrao@gmail.com>
2020-03-18 21:12:26 -0300

    execute_sql function

--- main/game.py
+++ main/game.py
@@ -5,18 +5,18 @@ import sqlite3
 class Game:
     def __init__(self):
         self.table_name = "games"
+        self.database = "database.db"
 
     def create_database(self):
-        conn = sqlite3.connect(database="database")
-        c = conn.cursor()
-        sql = "create table if not exists " + self.table_name + " (id integer)"
-        c.execute(sql)
-        conn.commit()
+        sql = "CREATE TABLE IF NOT EXISTS" + self.table_name  + "(game_id, player, X, Y)"
+        self.execute_sql
 
     def execute_sql(self, sql):
-        conn = sqlite3.connect(database="database")
+        conn = sqlite3.connect(database=self.database)
         c = conn.cursor()
         c.execute(sql)
+        c.execute("SELECT * FROM GAMES")
+        print(c.fetchone())
         conn.commit()
 
     def new_game(self):
@@ -25,4 +25,9 @@ class Game:
         response = [game_id, player]
         return response
 
+    def new_game(self):
+        database = sqlite3.connect("games.db")
+        cursor = database.cursor()
+
+Game().create_database()
 
